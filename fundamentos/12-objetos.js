0f130da1d9edcb5435c9c6f95321a305b133cd7b
Leonardo Gama <leogama09@gmail.com>
2022-02-14 16:00:31 -0300

    Criei o 12º Fundamento (objetos)

---
+++ fundamentos/12-objetos.js
@@ -0,0 +1,14 @@
+const prod1 ={}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40  //evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preço: 79.90 
+}
+
+'{ "nome": "Camisa Polo", "preço": 79.90 }'
+console.log(prod2)

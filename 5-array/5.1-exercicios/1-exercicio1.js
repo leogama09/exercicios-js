918b7fdb0e2a52fb4c035c4bf16a23ca78cb4fb1
Leonardo Gama <leogama09@gmail.com>
2022-02-24 14:36:26 -0300

    Fiz o exercício 1

---
+++ 5-array/5.1-exercicios/1-exercicio1.js
@@ -0,0 +1,13 @@
+/*
+Crie uma função que retorna a string "Olá, " concatenada com um argumento text 
+(a ser passado para a função) e com ponto de exclamação "!" no final.
+*/
+
+function cumprimentar(nome) {
+    return `Olá, ${nome}!`
+    console.log(`Olá, ${nome}!`)
+}
+
+console.log(cumprimentar("Leonardo"))
+
+    
